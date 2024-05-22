940245e8e52aefa3b956d10668edb93d1223dccd
kaylagarcia614 <130188636+kaylagarcia614@users.noreply.github.com>
2024-05-22 13:45:09 -0700

    built behavior tree, companion follows player

---
+++ Assets/Scripts/AI Scripts/CompanionAI.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class CompanionAI : MonoBehaviour
+{
+    [SerializeField] private float chasingRange;
+    [SerializeField] private float minChaseDistance; // New serialized field for minimum chase distance
+    [SerializeField] private Transform playerTransform;
+
+    private NavMeshAgent agent;
+    private Node topNode;
+
+    private void Awake()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    private void Start()
+    {
+        ConstructBehaviourTree();
+    }
+
+    private void ConstructBehaviourTree()
+    {
+        ChaseNode chaseNode = new ChaseNode(playerTransform, agent, this, minChaseDistance); // Pass the minimum distance
+        RangeNode chasingRangeNode = new RangeNode(chasingRange, playerTransform, transform);
+
+        Sequence chaseSequence = new Sequence(new List<Node> { chasingRangeNode, chaseNode });
+
+        topNode = chaseSequence;
+    }
+
+    private void Update()
+    {
+        topNode.Evaluate();
+        if (topNode.nodeState == NodeState.FAILURE)
+        {
+            agent.isStopped = true;
+        }
+    }
+}
---
+++ Assets/Scripts/AI Scripts/Nodes/ChaseNode.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class ChaseNode : Node
+{
+    private Transform target;
+    private NavMeshAgent agent;
+    private CompanionAI ai;
+    private float minDistance; // New minimum distance
+
+    public ChaseNode(Transform target, NavMeshAgent agent, CompanionAI ai, float minDistance)
+    {
+        this.target = target;
+        this.agent = agent;
+        this.ai = ai;
+        this.minDistance = minDistance; // Initialize the minimum distance
+    }
+
+    public override NodeState Evaluate()
+    {
+        float distance = Vector3.Distance(target.position, agent.transform.position);
+        if (distance > minDistance) // Use the minimum distance
+        {
+            agent.isStopped = false;
+            agent.SetDestination(target.position);
+            return NodeState.RUNNING;
+        }
+        else
+        {
+            agent.isStopped = true;
+            return NodeState.SUCCESS;
+        }
+    }
+}
