b0b4fdf1902aaeb0d4d4f1acae6a44a2d9bb8150
Fallrain1763 <liushuchen842@icloud.com>
2024-05-22 21:11:26 -0700

    add PlayerOnPlate

--- Assets/Scripts/PlateTrigger.cs
+++ Assets/Scripts/PlateTrigger.cs
@@ -7,21 +7,36 @@ public class PlateTrigger : MonoBehaviour
     [SerializeField] Material orange;
     [SerializeField] Material green;
 
+    void Awake()
+    {
+        PlayerPrefs.SetInt("PlayerOnPlate", 0);
+    }
+
     private void OnTriggerEnter(Collider collision)
     {
-        if (collision.gameObject.tag == "Player")
+        if (collision.gameObject.tag == "Player" || collision.gameObject.tag == "Companion")
         {
             gameObject.GetComponent <Renderer>().material = green;
             gameObject.tag = "on";
+
+            if (collision.gameObject.tag == "Player")
+            {
+                PlayerPrefs.SetInt("PlayerOnPlate", 1);
+            }
         }
     }
 
     private void OnTriggerExit(Collider collision)
     {
-        if (collision.gameObject.tag == "Player")
+        if (collision.gameObject.tag == "Player" || collision.gameObject.tag == "Companion")
         {
             gameObject.GetComponent<Renderer>().material = orange;
             gameObject.tag = "off";
+
+            if (collision.gameObject.tag == "Player")
+            {
+                PlayerPrefs.SetInt("PlayerOnPlate", 0);
+            }
         }
     }
 }
